cbcb598b3e5a641fdc0f4da4d02ae6bbeb270329
Enesbil <enesbilek23@gmail.com>
2026-01-07 23:14:17 +0300

    render posts using js instead

--- Oldagram (Insta UI Clone)/index.js
+++ Oldagram (Insta UI Clone)/index.js
@@ -28,3 +28,31 @@ const posts = [
     }
 ]
 
+const body = document.querySelector("body")
+
+function renderPosts(arr){
+    for(let i = 0; i < arr.length; i ++){
+        body.innerHTML += `        
+            <section class="post-body">
+                <div class="post-header">
+                    <img class ="poster-icon" alt="O.P. icon" src=${arr[i].avatar}>
+                    <div class="op-info">
+                        <p class="op-name">${arr[i].name}</p>
+                        <p class="op-location">${arr[i].location}</p>
+                    </div>
+                </div>
+                <img class="post-image" src=${arr[i].post}>
+                <div class="post-cta">
+                    <img class="cta-icon" src="images/icon-heart.png">
+                    <img class="cta-icon" src="images/icon-comment.png">
+                    <img class="cta-icon" src="images/icon-dm.png">
+                </div>
+                <div class="post-bottom">
+                    <p class="bottom-bold">${arr[i].likes} likes</p>
+                    <p class="bottom-regular"><span class="bottom-bold">${arr[i].username}</span> ${arr[i].comment}</p>
+                </div>
+            </section>`
+    }
+}
+
+renderPosts(posts)
